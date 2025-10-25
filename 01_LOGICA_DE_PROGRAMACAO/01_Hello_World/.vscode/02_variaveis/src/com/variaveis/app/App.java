5e868e54dddcab1c25588ae598264dbe55c18d34
Hedlly <hedlly26@gmail.com>
2025-10-24 21:34:14 -0300

    meu primeiro commit

---
+++ 01_LOGICA_DE_PROGRAMACAO/01_Hello_World/.vscode/02_variaveis/src/com/variaveis/app/App.java
@@ -0,0 +1,15 @@
+package com.variaveis.app;
+public class App {
+    public static void main(String[] args) throws Exception {
+        String nome = "hedlly" ;
+        int idade = 38;
+        double altura = 1.83;
+
+        System.out.println ("Nome: " + nome);
+        System.out.println ("idade: " + idade);
+        System.out.println ("altura: " + altura + " m");
+
+
+
+    }
+}
---
+++ 01_LOGICA_DE_PROGRAMACAO/03_entrada/src/com/entrada/app/App.java
@@ -0,0 +1,38 @@
+
+
+import java.util.Scanner;
+public class App {
+
+    public static void main(String[] args) throws Exception {
+    //    instancia do objeto leia
+        java.util.Scanner leia = new Scanner (System.in);
+        
+        // declaracao de variaveis
+        String nome;
+        string email;
+        String idade;
+        double altura;
+
+        // entrada de dados
+        System.out.println("informe seu nome:");
+        nome = leia.nextLine();
+        System.out.println("informe sua idade:");
+        idade = leia.nextLine();
+        System.out.println( "informe sua altura em metros:");
+        altura = leia.nextLine();
+        System.out.println(" informe seu email:");
+        email = leia.nextLine()
+
+        // saida de dados 
+        System.out.println("Mome: " +nome);
+        System.out.println("Idade: " +idade);
+        System.out.println( "altura: " +altura);
+
+        // limpexa de buffer
+       leia.nextLine();
+    
+      
+
+    }
+        
+}
